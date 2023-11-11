6569db0e9d89f49160a938ba18c11a5bd3eb8594
ReflexLevel0 <dominikdejanovic2@gmail.com>
2023-11-11 11:45:12 +0100

    Added search form

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <RestaurantTable/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RestaurantTable from "@/components/RestaurantTable.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    RestaurantTable
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/dist/css/bootstrap.css";
 </style>
--- website/src/components/RestaurantTable.vue
+++ website/src/components/RestaurantTable.vue
@@ -1,11 +1,60 @@
-<script setup>
-
+<script>
+export default {
+  data() {
+    return {
+      searchOption: "all",
+      searchCategories: [
+        {value: "all", text: "All fields"},
+        {value: "name", text: "Name"},
+        {value: "address", text: "Address"},
+        {value: "zipcode", text: "Zipcode"},
+        {value: "latitude", text: "Latitude"},
+        {value: "longitude", text: "Longitude"},
+        {value: "phone", text: "Phone"},
+        {value: "opening_hours", text: "Opening hours"},
+        {value: "delivery", text: "Delivery"},
+        {value: "city", text: "City"},
+        {value: "linkType", text: "Link type"},
+        {value: "link", text: "Link"},
+      ],
+      searchText: "",
+      restaurants: []
+    }
+  },
+  methods: {
+    refreshData(){
+      alert(`search category ${this.$data.searchOption} for text "${this.$data.searchText}"`)
+    }
+  }
+}
 </script>
 
 <template>
 
+  <form>
+    <input class="input-group" type="text" placeholder="Zagreb" v-model="$data.searchText"/>
+    <select class="form-select" v-model="$data.searchOption">
+      <option v-for="category in searchCategories" :value=category.value>{{category.text}}</option>
+    </select>
+    <button class="btn btn-primary" type="submit" @click="refreshData">Search</button>
+  </form>
+
+  <table>
+    <tr id="searchSelector" v-for="restaurant in restaurants">
+      <td>{{ restaurant.name }}</td>
+    </tr>
+  </table>
+
 </template>
 
 <style scoped>
-
+form{
+  width: 250px;
+}
+form>input{
+  margin-bottom: 10px;
+}
+form>select{
+  margin-bottom: 10px;
+}
 </style>
