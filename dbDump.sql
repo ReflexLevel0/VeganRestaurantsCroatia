0b23379f427c277db539288c7ab96f25b11890c4
ReflexLevel0 <dominikdejanovic2@gmail.com>
2023-11-13 22:32:00 +0100

    Changed copy commands to insert in dbDump.sql

--- dbDump.sql
+++ dbDump.sql
@@ -16,6 +16,29 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+DROP DATABASE "veganCroatia";
+--
+-- Name: veganCroatia; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE "veganCroatia" WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDER = libc LOCALE = 'hr_HR.UTF-8';
+
+
+ALTER DATABASE "veganCroatia" OWNER TO postgres;
+
+\connect "veganCroatia"
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
 --
 -- Name: linkexporttype; Type: TYPE; Schema: public; Owner: postgres
 --
@@ -236,107 +259,99 @@ ALTER TABLE public.restaurantcsvview OWNER TO postgres;
 -- Data for Name: city; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.city (id, name) FROM stdin;
-1	Zagreb
-2	Split
-3	Zadar
-4	Dubrovnik
-5	Bol
-6	Pag
-\.
+INSERT INTO public.city VALUES (1, 'Zagreb');
+INSERT INTO public.city VALUES (2, 'Split');
+INSERT INTO public.city VALUES (3, 'Zadar');
+INSERT INTO public.city VALUES (4, 'Dubrovnik');
+INSERT INTO public.city VALUES (5, 'Bol');
+INSERT INTO public.city VALUES (6, 'Pag');
 
 
 --
 -- Data for Name: link; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.link (id, type) FROM stdin;
-1	website
-2	facebook
-3	instagram
-4	youtube
-5	tiktok
-6	linkedin
-7	tripadvisor
-8	X
-\.
+INSERT INTO public.link VALUES (1, 'website');
+INSERT INTO public.link VALUES (2, 'facebook');
+INSERT INTO public.link VALUES (3, 'instagram');
+INSERT INTO public.link VALUES (4, 'youtube');
+INSERT INTO public.link VALUES (5, 'tiktok');
+INSERT INTO public.link VALUES (6, 'linkedin');
+INSERT INTO public.link VALUES (7, 'tripadvisor');
+INSERT INTO public.link VALUES (8, 'X');
 
 
 --
 -- Data for Name: restaurant; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.restaurant (id, name, address, cityid, zipcode, latitude, longitude, phone, opening_hours, delivery) FROM stdin;
-1	Restoran Vegehop	Vlaška ulica 79	1	10000	45.81462343	15.98823879	014649400	Mon-Sat 12:00-20:00	t
-2	Gajbica	Vlaška ulica 7	1	10000	45.81391498	15.97946271	0915141274	Mon-Sat 11:00-18:00	t
-3	OAZA Joyful Kitchen	Ulica Pavla Radića 9	1	10000	45.81434540	15.97565004	0976602744	Mon-Fri 10:00-21:00,Sat 12:00-21:00	t
-4	BEKIND	Ilica 75	1	10000	45.81245345	15.96450714	015534763	Tue-Sat 09:00-22:00,Sun 09:00-16:00	f
-5	Zrno bio bistro	Medulićeva ulica 20	1	10000	45.81122113	15.96667598	014847540	Mon-Sat 12:00-21:30	t
-6	Falafel, etc.	Ulica Andrije Žaje 60	1	10000	45.80695890	15.95334024	012343945	Mon-Sun 11:00-21:30	t
-7	Shambala	Iločka ulica 34	1	10000	45.79956603	15.95967074	0957618710	Mon-Fri 11:00-18:00,Sat-Sun 11:00:16:00	t
-8	Simple Green by Jelena	Zelinska Ulica 7	1	10000	45.80106354	15.97246634	015561679	Mon-Sat 08:00-16:00	t
-9	Vege Fino za sve	Ulica Lavoslava Ružičke 48	1	10000	45.79626824	15.96986333	098777577	Mon-Fri 10:00-18:00,Sun 12:00-18:00	t
-10	Barcode Mitra	Zagrebačka cesta 113	1	10000	45.80673849	15.92470464	013770428	Mon-Sat 14:00-22:00	t
-11	Pandora Greenbox	Obrov ulica 4	2	21000	43.50930416	16.43764950	021236120	Mon-Sun 08:30-12:00	f
-12	Upcafe	Ulica Domovinskog rata 29a	2	21000	43.51692872	16.44540914	0916210500	Mon-Sat 07:00-20:00,Sun 08:00-20:00	t
-13	The Botanist	Ulica Mihovila Pavlinovića 4	3	23000	44.11643740	15.22593932	0924232296	Mon-Sun 12:00-23:00	f
-14	Nishta	Prijeko ulica bb	4	20000	42.67534972	18.10592518	020322088	Mon-Sat 11:30-22:00	f
-15	BioMania Bistro Bol	Rudina 10	5	21420	43.26175996	16.65493408	0919362276	Mon-Thu 13:00-21:00,Sat 13:00-21:00,Sun 10:00-22:00	f
-\.
+INSERT INTO public.restaurant VALUES (1, 'Restoran Vegehop', 'Vlaška ulica 79', 1, 10000, 45.81462343, 15.98823879, '014649400', 'Mon-Sat 12:00-20:00', true);
+INSERT INTO public.restaurant VALUES (2, 'Gajbica', 'Vlaška ulica 7', 1, 10000, 45.81391498, 15.97946271, '0915141274', 'Mon-Sat 11:00-18:00', true);
+INSERT INTO public.restaurant VALUES (3, 'OAZA Joyful Kitchen', 'Ulica Pavla Radića 9', 1, 10000, 45.81434540, 15.97565004, '0976602744', 'Mon-Fri 10:00-21:00,Sat 12:00-21:00', true);
+INSERT INTO public.restaurant VALUES (4, 'BEKIND', 'Ilica 75', 1, 10000, 45.81245345, 15.96450714, '015534763', 'Tue-Sat 09:00-22:00,Sun 09:00-16:00', false);
+INSERT INTO public.restaurant VALUES (5, 'Zrno bio bistro', 'Medulićeva ulica 20', 1, 10000, 45.81122113, 15.96667598, '014847540', 'Mon-Sat 12:00-21:30', true);
+INSERT INTO public.restaurant VALUES (6, 'Falafel, etc.', 'Ulica Andrije Žaje 60', 1, 10000, 45.80695890, 15.95334024, '012343945', 'Mon-Sun 11:00-21:30', true);
+INSERT INTO public.restaurant VALUES (7, 'Shambala', 'Iločka ulica 34', 1, 10000, 45.79956603, 15.95967074, '0957618710', 'Mon-Fri 11:00-18:00,Sat-Sun 11:00:16:00', true);
+INSERT INTO public.restaurant VALUES (8, 'Simple Green by Jelena', 'Zelinska Ulica 7', 1, 10000, 45.80106354, 15.97246634, '015561679', 'Mon-Sat 08:00-16:00', true);
+INSERT INTO public.restaurant VALUES (9, 'Vege Fino za sve', 'Ulica Lavoslava Ružičke 48', 1, 10000, 45.79626824, 15.96986333, '098777577', 'Mon-Fri 10:00-18:00,Sun 12:00-18:00', true);
+INSERT INTO public.restaurant VALUES (10, 'Barcode Mitra', 'Zagrebačka cesta 113', 1, 10000, 45.80673849, 15.92470464, '013770428', 'Mon-Sat 14:00-22:00', true);
+INSERT INTO public.restaurant VALUES (11, 'Pandora Greenbox', 'Obrov ulica 4', 2, 21000, 43.50930416, 16.43764950, '021236120', 'Mon-Sun 08:30-12:00', false);
+INSERT INTO public.restaurant VALUES (12, 'Upcafe', 'Ulica Domovinskog rata 29a', 2, 21000, 43.51692872, 16.44540914, '0916210500', 'Mon-Sat 07:00-20:00,Sun 08:00-20:00', true);
+INSERT INTO public.restaurant VALUES (13, 'The Botanist', 'Ulica Mihovila Pavlinovića 4', 3, 23000, 44.11643740, 15.22593932, '0924232296', 'Mon-Sun 12:00-23:00', false);
+INSERT INTO public.restaurant VALUES (14, 'Nishta', 'Prijeko ulica bb', 4, 20000, 42.67534972, 18.10592518, '020322088', 'Mon-Sat 11:30-22:00', false);
+INSERT INTO public.restaurant VALUES (15, 'BioMania Bistro Bol', 'Rudina 10', 5, 21420, 43.26175996, 16.65493408, '0919362276', 'Mon-Thu 13:00-21:00,Sat 13:00-21:00,Sun 10:00-22:00', false);
 
 
 --
 -- Data for Name: restaurantlink; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-COPY public.restaurantlink (restaurantid, linktype, link) FROM stdin;
-1	1	vegehop.hr
-1	2	facebook.com/vegehop
-1	3	instagram.com/vegehop_restaurant
-1	4	youtube.com/user/Vegehop
-2	1	gajbica.eatbu.com
-2	2	facebook.com/gajbicahr
-2	3	instagram.com/gajbica_zg
-3	1	joyful-kitchen.com
-3	2	facebook.com/oaza.joyful.kitchen
-3	3	instagram.com/oaza.joyful.kitchen
-3	5	tiktok.com/@oaza.joyful.kitchen
-3	6	linkedin.com/company/oaza-joyful-kitchen
-4	1	bekind.hr
-4	2	facebook.com/bekind.zagreb
-4	3	instagram.com/bekind.zagreb
-5	1	zrnobiobistro.hr
-5	2	facebook.com/zrnobiobistro
-5	3	instagram.com/zrnobiobistro
-6	1	falafeletc.com.hr
-6	2	facebook.com/falafeletczg
-7	2	facebook.com/people/Shambala-Vegan-food-sweets-more/100083364727743
-8	1	simplegreenbyjelena.com
-8	2	facebook.com/profile.php?id=100057104513129
-8	3	instagram.com/simplegreen_by_jelena
-8	7	tripadvisor.com/Restaurant_Review-g294454-d11844934-Reviews-Simple_Green_Bake_by_Jelena-Zagreb_Central_Croatia.html
-9	1	vegefinozasve.com
-9	2	facebook.com/VegeFinoZaSve
-10	1	barcodemitra.hr
-10	2	facebook.com/BarcodeMitra
-10	3	instagram.com/barcode_mitra
-11	1	pandoragreenbox.com
-11	2	facebook.com/PandoraGreenbox
-11	3	instagram.com/pandora_greenbox
-12	1	upcafe.hr
-12	2	facebook.com/upcafe12
-12	3	instagram.com/upcafe.split
-12	7	tripadvisor.com/Restaurant_Review-g295370-d5773474-Reviews-UPcafe-Split_Split_Dalmatia_County_Dalmatia.html
-13	3	instagram.com/botanist_zadar
-14	1	nishtarestaurant.com
-14	2	facebook.com/nishtarestaurant
-14	3	instagram.com/explore/locations/798999121
-14	8	twitter.com/hashtag/nishta
-15	1	biomania.hr
-15	2	facebook.com/biomaniahr
-15	3	instagram.com/biomaniahr
-15	7	tripadvisor.com/Restaurant_Review-g303802-d14997286-Reviews-BioMania_Street_Food-Bol_Brac_Island_Split_Dalmatia_County_Dalmatia.html
-\.
+INSERT INTO public.restaurantlink VALUES (1, 1, 'vegehop.hr');
+INSERT INTO public.restaurantlink VALUES (1, 2, 'facebook.com/vegehop');
+INSERT INTO public.restaurantlink VALUES (1, 3, 'instagram.com/vegehop_restaurant');
+INSERT INTO public.restaurantlink VALUES (1, 4, 'youtube.com/user/Vegehop');
+INSERT INTO public.restaurantlink VALUES (2, 1, 'gajbica.eatbu.com');
+INSERT INTO public.restaurantlink VALUES (2, 2, 'facebook.com/gajbicahr');
+INSERT INTO public.restaurantlink VALUES (2, 3, 'instagram.com/gajbica_zg');
+INSERT INTO public.restaurantlink VALUES (3, 1, 'joyful-kitchen.com');
+INSERT INTO public.restaurantlink VALUES (3, 2, 'facebook.com/oaza.joyful.kitchen');
+INSERT INTO public.restaurantlink VALUES (3, 3, 'instagram.com/oaza.joyful.kitchen');
+INSERT INTO public.restaurantlink VALUES (3, 5, 'tiktok.com/@oaza.joyful.kitchen');
+INSERT INTO public.restaurantlink VALUES (3, 6, 'linkedin.com/company/oaza-joyful-kitchen');
+INSERT INTO public.restaurantlink VALUES (4, 1, 'bekind.hr');
+INSERT INTO public.restaurantlink VALUES (4, 2, 'facebook.com/bekind.zagreb');
+INSERT INTO public.restaurantlink VALUES (4, 3, 'instagram.com/bekind.zagreb');
+INSERT INTO public.restaurantlink VALUES (5, 1, 'zrnobiobistro.hr');
+INSERT INTO public.restaurantlink VALUES (5, 2, 'facebook.com/zrnobiobistro');
+INSERT INTO public.restaurantlink VALUES (5, 3, 'instagram.com/zrnobiobistro');
+INSERT INTO public.restaurantlink VALUES (6, 1, 'falafeletc.com.hr');
+INSERT INTO public.restaurantlink VALUES (6, 2, 'facebook.com/falafeletczg');
+INSERT INTO public.restaurantlink VALUES (7, 2, 'facebook.com/people/Shambala-Vegan-food-sweets-more/100083364727743');
+INSERT INTO public.restaurantlink VALUES (8, 1, 'simplegreenbyjelena.com');
+INSERT INTO public.restaurantlink VALUES (8, 2, 'facebook.com/profile.php?id=100057104513129');
+INSERT INTO public.restaurantlink VALUES (8, 3, 'instagram.com/simplegreen_by_jelena');
+INSERT INTO public.restaurantlink VALUES (8, 7, 'tripadvisor.com/Restaurant_Review-g294454-d11844934-Reviews-Simple_Green_Bake_by_Jelena-Zagreb_Central_Croatia.html');
+INSERT INTO public.restaurantlink VALUES (9, 1, 'vegefinozasve.com');
+INSERT INTO public.restaurantlink VALUES (9, 2, 'facebook.com/VegeFinoZaSve');
+INSERT INTO public.restaurantlink VALUES (10, 1, 'barcodemitra.hr');
+INSERT INTO public.restaurantlink VALUES (10, 2, 'facebook.com/BarcodeMitra');
+INSERT INTO public.restaurantlink VALUES (10, 3, 'instagram.com/barcode_mitra');
+INSERT INTO public.restaurantlink VALUES (11, 1, 'pandoragreenbox.com');
+INSERT INTO public.restaurantlink VALUES (11, 2, 'facebook.com/PandoraGreenbox');
+INSERT INTO public.restaurantlink VALUES (11, 3, 'instagram.com/pandora_greenbox');
+INSERT INTO public.restaurantlink VALUES (12, 1, 'upcafe.hr');
+INSERT INTO public.restaurantlink VALUES (12, 2, 'facebook.com/upcafe12');
+INSERT INTO public.restaurantlink VALUES (12, 3, 'instagram.com/upcafe.split');
+INSERT INTO public.restaurantlink VALUES (12, 7, 'tripadvisor.com/Restaurant_Review-g295370-d5773474-Reviews-UPcafe-Split_Split_Dalmatia_County_Dalmatia.html');
+INSERT INTO public.restaurantlink VALUES (13, 3, 'instagram.com/botanist_zadar');
+INSERT INTO public.restaurantlink VALUES (14, 1, 'nishtarestaurant.com');
+INSERT INTO public.restaurantlink VALUES (14, 2, 'facebook.com/nishtarestaurant');
+INSERT INTO public.restaurantlink VALUES (14, 3, 'instagram.com/explore/locations/798999121');
+INSERT INTO public.restaurantlink VALUES (14, 8, 'twitter.com/hashtag/nishta');
+INSERT INTO public.restaurantlink VALUES (15, 1, 'biomania.hr');
+INSERT INTO public.restaurantlink VALUES (15, 2, 'facebook.com/biomaniahr');
+INSERT INTO public.restaurantlink VALUES (15, 3, 'instagram.com/biomaniahr');
+INSERT INTO public.restaurantlink VALUES (15, 7, 'tripadvisor.com/Restaurant_Review-g303802-d14997286-Reviews-BioMania_Street_Food-Bol_Brac_Island_Split_Dalmatia_County_Dalmatia.html');
 
 
 --
